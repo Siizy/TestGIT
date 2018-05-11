b8ee822da52b8cc25a850d96f7200c2913b19d67
Mamta <Mamta@DESKTOP-L0EM54K>
2018-05-11 08:25:21 +0530

    Initial changes

---
+++ TestGit/src/Class1.java
@@ -0,0 +1,16 @@
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		test1();
+
+	}
+	
+	public static void test1() {
+		
+		System.out.println("Hi I am Chandan");
+		System.out.println("I work for TSYS");
+		
+	}
+
+}
